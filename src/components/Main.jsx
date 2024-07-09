37a5af8b47b67fb35367cb295bb070cddb0b0c15
D3k1 <dofodofonson@gmail.com>
2024-07-09 21:57:54 +0300

    ntc

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -131,7 +131,7 @@ const TABS = {
   },
 };
 
-for (let i = 0; i < 5; ++i) {
+for (let i = 0; i < 6; ++i) {
   TABS.all.items = [].concat(TABS.all.items, TABS.all.items);
 }
 const TABS_KEYS = Object.keys(TABS);
